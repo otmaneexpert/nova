10e67663fceb59aa88146c9e09d0d55e37f0249f
IGAyoubArt <ig.ayoub.dev@gmail.com>
2026-01-27 23:51:40 +0100

    add all

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -20,6 +20,19 @@ const WhatsAppButton = () => {
       `}
       aria-label="WhatsApp"
     >
+      <span
+        className={`absolute -top-1
+          ${isRTL ? "-left-1" : "-right-1"}
+          w-5 h-5
+          bg-red-500 text-white
+          text-xs font-bold
+          rounded-full
+          flex items-center justify-center
+          shadow
+        `}
+      >
+        1
+      </span>
       <svg
         viewBox="0 0 32 32"
         className="w-7 h-7 fill-white"
